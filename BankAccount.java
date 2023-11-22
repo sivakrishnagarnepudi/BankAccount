81c5a3d47e89189fe0e70d162aeb660c56208123
sivakrishnagarnepudi <gsivakrishna007@gmail.com>
2023-11-22 15:05:00 +0530

    Bank Account Details

---
+++ BankAccount.java
@@ -0,0 +1,17 @@
+public class BankAccount {
+        public static void main(String[] args) {
+            String bank = "SBI";
+            if(bank == "HDFC") {
+                System.out.println("bank  is HDFC");
+            }else if (bank == "Axis") {
+                System.out.println("bank  is AXIS");
+            }else if(bank == "CANARA") {
+                System.out.println("bank  is CANARA");
+            }else if(bank=="SBI"){
+                System.out.println("bank is STATE BANK OF INDIA");
+            }else {
+                System.out.println(bank);
+            }
+        }
+    }
+
